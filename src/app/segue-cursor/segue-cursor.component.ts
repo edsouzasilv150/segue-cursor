999716cdee12043be682d34e634b0232879e6261
Edilson Sousa da Silva <edilson.silva@tty2000.com.br>
2024-03-25 09:36:51 -0300

    create logic segue-cursor

--- src/app/segue-cursor/segue-cursor.component.ts
+++ src/app/segue-cursor/segue-cursor.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { delay, map, filter } from 'rxjs/operators'
+export interface ICoordinates {
+    x: number,
+    y: number,
+}
 
 @Component({
-  selector: 'app-segue-cursor',
+  selector: 'segue-cursor',
   templateUrl: './segue-cursor.component.html',
   styleUrls: ['./segue-cursor.component.css']
 })
-export class SegueCursorComponent implements OnInit {
+export class SegueCursorComponent implements OnInit, AfterViewInit {
+  circle!: HTMLElement
 
-  constructor() { }
+  coordinates = fromEvent<MouseEvent>(document, 'mousemove').pipe(
+    map((event) => ({ x: event.clientX, y: event.clientY })),
+    filter((value) => value.x > 325 && value.x < 1022),
+    delay(800)
+  );
 
-  ngOnInit(): void {
+
+  onNext(value: ICoordinates) {
+    if(this.circle) {
+      this.circle.style.left = `${value.x}px`
+      this.circle.style.top = `${value.y}px`
+    }
   }
 
+  constructor() {}
+
+  ngOnInit() {
+    this.coordinates.subscribe({
+      next: (value: ICoordinates) => this.onNext(value),
+      error: (e: Error) => console.log(e),
+      complete: () =>console.log(),
+    })
+  }
+
+  ngAfterViewInit() {
+    this.circle = document.getElementById('circle') as HTMLElement
+  }
 }
