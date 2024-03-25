295105f8bfa1fba381f79bf5cc1bf5c5b037126b
Edilson Sousa da Silva <edilson.silva@tty2000.com.br>
2024-03-25 10:50:00 -0300

    create test unitary

--- src/app/segue-cursor/segue-cursor.component.spec.ts
+++ src/app/segue-cursor/segue-cursor.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable} from 'rxjs'
 
 import { SegueCursorComponent } from './segue-cursor.component';
 
@@ -19,5 +20,47 @@ describe('SegueCursorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update circle position on onNext', () =>{
+    const mockCoordinates = { x: 100, y: 100 };
+    const circleMock = {
+      style: {
+        left: '',
+        top: ''
+      }
+    } as HTMLElement;
+    component.circle = circleMock;
+
+    component.onNext(mockCoordinates);
+
+    expect(circleMock.style.left).toBe(`${mockCoordinates.x}px`);
+    expect(circleMock.style.top).toBe(`${mockCoordinates.y}px`);
+  });
+
+  it('should subscribe to mousemove event on ngOnInit', () => {
+    const fakeMouseEvent = new MouseEvent('mousemove', {
+      clientX: 200,
+      clientY: 200
+    });
+    spyOn(component.coordinates as Observable<MouseEvent>, 'subscribe').and.callThrough();
+    spyOn(component, 'onNext');
+
+    component.ngOnInit();
+    component.coordinates.subscribe();
+
+    component.coordinates.subscribe((coordinates: any) => {
+      expect(component.onNext).toHaveBeenCalledWith({ x: fakeMouseEvent.clientX, y:fakeMouseEvent.clientY });
+    });
+  });
+
+  it('should set circle element on ngAfterViewInit', () => {
+    const circleElement = document.createElement('div');
+    circleElement.id = 'circle';
+    spyOn(document, 'getElementById').and.returnValue(circleElement);
+
+    component.ngAfterViewInit();
+
+    expect(component.circle).toBe(circleElement);
   });
 });
